50b21ff50af8fbe554afa7246edd6094a24a6ae2
Lise Ferguson <lise.ferguson503@gmail.com>
2018-05-23 17:07:45 -0700

    added js and jquery functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getDataFromApi(searchTerm, callback) {
     data: {
      q: `${searchTerm} in: name`,
       part: 'snippet',
-      key: 'AIzaSyAKzTbFtHT2upbhqWjy_OVYBzTZhmgx48Y ',
+      key: 'AIzaSyAKzTbFtHT2upbhqWjy_OVYBzTZhmgx48Y',
       per_page: 15
   },
   dataType: 'json',
@@ -18,3 +18,37 @@ function getDataFromApi(searchTerm, callback) {
 
 $.ajax(settings);
 }
+
+// getDataFromApi('cats', (response) =>{console.log(response)})
+
+//this function renders the results of the search including the video title, source, and number of views
+function renderResult(result){
+  return `
+    <div>
+      <h2>
+      <a class="js-result-name" href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a> by 
+      <a class="js-video-author" href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank">${result.snippet.channelTitle}</a></h2>
+      <div class="image"> 
+        <img src="${result.snippet.thumbnails.default.url}">
+      </div>
+    </div>
+  `;
+}
+
+function displayYouTubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+$(watchSubmit);
